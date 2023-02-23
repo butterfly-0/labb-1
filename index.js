8c010424e714d302efbc3f0376f787eeadf14983
Едуард Лукавський <lukavskyiedik@gmail.com>
2023-02-23 18:11:19 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,4 @@
+let name = prompt("Будь ласка, введіть своє ім'я:")
+let lastName = prompt("Будь ласка, введіть своє прізвище:")
+let age = prompt("Будь ласка, введіть свій вік:")
+console.log(`Мене звати ${name} ${lastName} та мені ${age} років !!!`)
